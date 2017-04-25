6806c7837d2cf84c363dafdf7a420432764ce41d
Asmaroth <bellicault.corentin@hotmail.fr>
2017-04-26 00:16:41 +0200

    Première version de media.hpp/cpp

--- media.cpp
+++ media.cpp
@@ -1,21 +1,21 @@
 #include "media.hpp"
 
 media::media(){
+	id = 0;
 	titre = "Aucun";
     auteur = "Inconnu";
     annee = -1;
     disponible = 2;
-    stock = 0;
     dureeEmprunt = -1;
     dateEmprunt = -1;
 }
 
-media::media(std::string _titre, std::string _auteur, int _annee, int _disponible, int _stock, int _dureeEmprunt, int _dateEmprunt){
+media::media(int _id, std::string _titre, std::string _auteur, int _annee, int _disponible, int _dureeEmprunt, int _dateEmprunt){
+	id = _id;
 	titre = _titre;
 	auteur = _auteur;
 	annee = _annee;
 	disponible = _disponible;
-	stock = _stock;
 	dureeEmprunt = _dureeEmprunt;
 	dateEmprunt = _dateEmprunt;
 }
@@ -28,10 +28,100 @@ void media::reserver(){
 
 void media::emprunter(){
 	disponible = 1;
-	stock--;
 }
 
 void media::rendre(){
 	disponible = 3;
-	stock++;
+}
+
+void media::setId(int _id){
+	id = _id;
+}
+
+int media::getId(){
+	return id;
+}
+
+void media::setTitre (std::string _titre){
+	titre = _titre;
+}
+
+std::string media::getTitre (){
+	return titre;
+}
+
+void media::setAuteur (std::string _auteur){
+	auteur = _auteur;
+}
+
+std::string media::getAuteur (){
+	return auteur;
+}
+
+void media::setAnnee (int _annee){
+	annee = _annee;
+}
+
+int media::getAnnee (){
+	return annee;
+}
+
+void media::setDisponible (int _disponible){
+	disponible = _disponible;
+}
+
+int media::getDisponible (){
+	return disponible;
+}
+
+void media::setDureeEmprunt (int _dureeEmprunt){
+	dureeEmprunt = _dureeEmprunt;
+}
+
+int media::getDureeEmprunt (){
+	return dureeEmprunt;
+}
+
+void media::setDateEmprunt (int _dateEmprunt){
+	dateEmprunt = _dateEmprunt;
+}
+
+int media::getDateEmprunt (){
+	return dateEmprunt;
+}
+
+void media::setIdClient(int _idClient){
+	idClient = _idClient;
+}
+
+int media::getIdClient(){
+	return idClient;
+}
+
+void media::info(){
+	std::string str;
+	if (disponible == 0)
+		str = "réservé";
+	else if (disponible == 1)
+		str = "emprunté";
+	else if (disponible == 2)
+		str = "indisponible";
+	else
+		str = "disponible";
+	std::cout << "(" << id << ") " << titre << ", de " << auteur << " est paru en " << annee << ". Il est actuellement " << str << ".\n" << std::endl;
+}
+
+void media::infoAdmin(){
+	/*std::string str;
+	if (disponible == 0)
+		str = "réservé";
+	else if (disponible == 1)
+		str = "emprunté";
+	else if (disponible == 2)
+		str = "disponible";
+	else
+		str = "indisponible";
+	std::cout << "(" << id << ") " << titre << ", de " << auteur << "est paru en " << annee << ". Il est actuellement " << str << ".\n" << std::endl;*/
+	info();
+	std::cout << "Ce media a été emprunté par (" << idClient << ") le " << dateEmprunt << " pour une durée maximale de " << dureeEmprunt << ".\n" << std::endl;	
 }
--- media.hpp
+++ media.hpp
@@ -5,39 +5,44 @@
  * \brief Contient toutes les ressources de la médiathèque
  *        Ajouter un "info" ou pas ?
  */
+
 class media {
     // Attributes
     protected :
+        int id;
         std::string titre;
         std::string auteur;
         int annee;
-        int disponible;
-        int stock;
+        int disponible;     //0 = réservé, 1 = emprunté, 2 = non disponible (utile ?), 3 = disponible
         int dureeEmprunt;
         int dateEmprunt;
+        int idClient;
     // Operations
     public :
         media();
-        media(std::string _titre, std::string _auteur, int _annee, int _disponible, int _stock, int _dureeEmprunt, int _dateEmprunt);
+        media(int _id, std::string _titre, std::string _auteur, int _annee, int _disponible, int _dureeEmprunt, int _dateEmprunt);
         ~media();
         void reserver ();
         void emprunter ();
         void rendre ();
+        void setId(int _id);
+        int getId();
         void setTitre (std::string _titre);
-        string getTitre ();
+        std::string getTitre ();
         void setAuteur (std::string _auteur);
-        string getAuteur ();
+        std::string getAuteur ();
         void setAnnee (int _annee);
         int getAnnee ();
         void setDisponible (int _disponible);
         int getDisponible ();
-        void setStock (int _stock);
-        int getStock ();
         void setDureeEmprunt (int _dureeEmprunt);
         int getDureeEmprunt ();
         void setDateEmprunt (int _dateEmprunt);
         int getDateEmprunt ();
+        void setIdClient(int _idClient);
+        int getIdClient();
         void info();
+        void infoAdmin();
 };
 
 #endif
